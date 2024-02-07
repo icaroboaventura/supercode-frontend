b39b906db5291dba0334adac899b6aa158227748
icaroboaventura <boaventura@outlook.de>
2024-02-06 21:20:29 -0300

    supercode

--- Aufgaben/2024_02/06.02.23_JS-Vertiefung-Project Mehrwertsteuer-Rechner-App/assets/js/main.js
+++ Aufgaben/2024_02/06.02.23_JS-Vertiefung-Project Mehrwertsteuer-Rechner-App/assets/js/main.js
@@ -2,26 +2,44 @@
 // Mehrwertsteuer-Rechner
 //----------------------------------------------------
 
+//----------------------------------------------------
+// text verändern jenach input radio
+//----------------------------------------------------
+
+const betragLabel = document.querySelector(".betrag");
+const endHeading = document.querySelector(".end-heading");
+const changeNetto = () => {
+  betragLabel.innerHTML =
+    "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro<span>*</span>";
+  endHeading.innerHTML = "Bruttobetrag (Endpreis)";
+};
+const changeBrutto = () => {
+  betragLabel.innerHTML =
+    "Bruttobetrag (Preis mit Mehrwertsteuer) in Euro<span>*</span>";
+  endHeading.innerHTML = "Nettobetrag (Endpreis)";
+};
+
+//----------------------------------------------------
+// Hauptfunktion - BERECHNEN
+//----------------------------------------------------
+
 const rechner = () => {
   const mehrwertsteuer = document.querySelector(
     'input[name="auf-oder-ab"]:checked'
   ).value;
-  const steuerSatz = document.querySelector('input[name="satz"]:checked').value;
+  const steuerSatz = Number(
+    document.querySelector('input[name="satz"]:checked').value
+  );
+  console.log(steuerSatz);
   const betrag = Number(document.querySelector("#betrag").value);
   const mwBetrag = document.querySelector(".mw-betrag");
   const endpreis = document.querySelector(".endpreis");
 
-  if (mehrwertsteuer === "aufschlagen" && steuerSatz === "neunzehn") {
-    mwBetrag.innerHTML = `${(betrag * 0.19).toFixed(2)} €`;
-    endpreis.innerHTML = `${(betrag + betrag * 0.19).toFixed(2)} €`;
-  } else if (mehrwertsteuer === "aufschlagen" && steuerSatz === "sieben") {
-    mwBetrag.innerHTML = `${(betrag * 0.07).toFixed(2)} €`;
-    endpreis.innerHTML = `${(betrag + betrag * 0.07).toFixed(2)} €`;
-  } else if (mehrwertsteuer === "abziehen" && steuerSatz === "neunzehn") {
-    mwBetrag.innerHTML = `${(betrag - betrag / 1.19).toFixed(2)} €`;
-    endpreis.innerHTML = `${(betrag / 1.19).toFixed(2)}  €`;
+  if (mehrwertsteuer === "aufschlagen") {
+    mwBetrag.innerHTML = `${(betrag * steuerSatz).toFixed(2)} €`;
+    endpreis.innerHTML = `${(betrag + betrag * steuerSatz).toFixed(2)} €`;
   } else {
-    mwBetrag.innerHTML = `${(betrag - betrag / 1.07).toFixed(2)} €`;
-    endpreis.innerHTML = `${(betrag / 1.07).toFixed(2)}  €`;
+    mwBetrag.innerHTML = `${(betrag - betrag / (steuerSatz + 1)).toFixed(2)} €`;
+    endpreis.innerHTML = `${(betrag / (steuerSatz + 1)).toFixed(2)}  €`;
   }
 };
